4abae560a20848255de3e55616a82e90f0c75c2f
twanlee <levantuan6898@gmail.com>
2020-08-10 16:06:59 +0700

    tạo funtion sign up

--- src/main/java/com/meo/mp3/controllers/RegisterController.java
+++ src/main/java/com/meo/mp3/controllers/RegisterController.java
@@ -3,6 +3,7 @@ package com.meo.mp3.controllers;
 import com.meo.mp3.models.users.account.User;
 import com.meo.mp3.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ public class RegisterController {
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody User user){
-
+        userService.singUp(user);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 }
